60af5a9f0e44280ee2c40d3ccd2f603e2f165e9b
jainvishal520 <vjtinu.jain431@gmail.com>
2019-04-12 01:27:57 +0530

    react router, link and navliknk

---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact = () => {
+    return(
+        <div className="container">
+            <h4 className="center">
+                Contact
+            </h4>
+            <p className="center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptatem eligendi est illo laudantium aspernatur, deserunt ipsum eveniet optio, fugiat pariatur ea magni quia quo natus inventore veniam! Blanditiis, perspiciatis.
+            </p>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className="container">
+            <h4 className="center">
+                Home
+            </h4>
+            <p className="center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptatem eligendi est illo laudantium aspernatur, deserunt ipsum eveniet optio, fugiat pariatur ea magni quia quo natus inventore veniam! Blanditiis, perspiciatis.
+            </p>
+        </div>
+    )
+}
+
+export default Home;
