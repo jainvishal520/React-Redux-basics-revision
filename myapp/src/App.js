d58ef3c20206efe1173e83a5b005b175c5f78df7
jainvishal520 <vjtinu.jain431@gmail.com>
2019-04-11 18:39:45 +0530

    looping through list

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 class App extends Component {
+  state={
+    ninjas: [
+      {name:'Vishal', age:25, belt:'black',id:1},
+      {name:'jarvis', age:30, belt:'blue',id:2},
+      {name:'siri', age:24, belt:'white',id:3}
+    ]
+  }
+
   render() {
     return (
       <div>
         <h1>hello</h1>
-        <Ninjas name="vishal" age="25" belt="black"/>
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
