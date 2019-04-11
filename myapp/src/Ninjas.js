059a1a8804ae29feb76e6b7e073b4b4a52fc6931
jainvishal520 <vjtinu.jain431@gmail.com>
2019-04-11 19:06:21 +0530

    conditional output

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -14,13 +14,13 @@ const Ninjas = ({ninjas}) => {
         //extract variables from props using destructuring
         // const {ninjas} = props;
         const ninjaList = ninjas.map((ninja) => {
-            return (
+            return ninja.age > 25 ?  (
                 <div className="ninja" key={ninja.id}>
                     <div>Name:  {ninja.name}</div>
                     <div>Age: {ninja.age}</div>
                     <div>Belt: {ninja.belt}</div>
                 </div>
-            )
+            ) : null
         })
         return(
             <div>
