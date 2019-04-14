bc79b4b97e63830b2597b1cd051022c8166a691a
jainvishal520 <vjtinu.jain431@gmail.com>
2019-04-14 18:31:36 +0530

    importing images

--- poketimes/src/components/posts.js
+++ poketimes/src/components/posts.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Pokeball from '../pokeball.png'
 const Posts = ({post}) => {
     return(
         <div className="container">
             <Link to={`/${post.id}`}>
+                <img src={Pokeball} alt=""/>
                 <h4>{post.title}</h4>
                 <p>{post.body}</p>
             </Link>
